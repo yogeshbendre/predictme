d1f2d7db053b84d1d00e27d2ab78fd6072b28686
yogeshbendre <07.yogesh@gmail.com>
2021-09-24 10:23:59 +0530

    Create ysbclassifier.py

---
+++ ysbclassifier.py
@@ -0,0 +1,127 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 100
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+#import matplotlib.pyplot as plt
+import numpy as np
+# functions to show an image
+
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+#imshow(torchvision.utils.make_grid(images))
+# print labels
+#print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+  net = net.cuda()
+  print("GPU Device found, congrats, you will have fast training...!!!")
+else:
+  print("No GPU found, training may be slow, hang on...!!!")
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+import time
+
+t0_total = time.time()
+total_epochs=5
+for epoch in range(total_epochs):  # loop over the dataset multiple times
+    t0 = time.time()
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        if use_cuda:
+          
+          inputs, labels = data[0].to('cuda'), data[1].to('cuda')
+        else:
+          
+          inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        #print("Batch: "+str(i))
+        running_loss += loss.item()
+        if (i+1) % 100 == 0:    
+            print('[Epoch %d, Batch %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    print('Epoch {} took {} seconds'.format((epoch+1), (time.time() - t0)))
+
+print('Finished Training')
+totalimages = len(trainloader.dataset)
+print('Training Stats')
+print('Number of images: '+str(totalimages))
+print('Number of images per batch: '+str(batch_size))
+print('Number of batches: '+str(totalimages/batch_size))
+print('Number of epochs: '+ str(total_epochs))
+print('Complete training took {} seconds'.format((time.time() - t0_total)))
+
+
+
+
+
+
